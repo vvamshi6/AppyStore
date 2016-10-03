c65bc90b63110147ba3f0a1aae9efdf6cf34b0d0
vvamshi6 <vvamshi6@gmail.com>
2016-10-03 13:21:07 +0530

    adding to git hub

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 */
 var express = require('express')
 ,app = express()
+,cors = require('cors')
 ,http = require('http').Server(app)
 ,port = process.env.PORT||4002
 ,bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static('./app'));
 app.use('/',function(req,res){
   console.log(req.url);
