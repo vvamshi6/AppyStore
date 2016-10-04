cbddecae6de27eca614682fdeb894d1482987898
vvamshi6 <vvamshi6@gmail.com>
2016-10-04 08:45:27 +0530

    adding to git

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(express.static('./app'));
 app.use('/',function(req,res){
   console.log(req.url);
 });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    if ('OPTIONS' === req.method) {
+        res.status(204).send();
+    }
+    else {
+        next();
+    }
+});
 app.listen(port,function(){
   console.log('server started at'+port);
 });
